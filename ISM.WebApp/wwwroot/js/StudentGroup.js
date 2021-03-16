b13e38799ddb754558e0b247069a2868a1a0a84f
BanNT <hoangtubongdem56@gmail.com>
2021-03-17 00:16:44 +0700

    update layout

--- ISM.WebApp/wwwroot/js/StudentGroup.js
+++ ISM.WebApp/wwwroot/js/StudentGroup.js
@@ -137,3 +137,22 @@ function deleteCoordinator(id) {
     rebuildSelectCoordinator();
     rebuildCoordinatorTable();
 }
+
+function myFunction1() {
+    document.getElementById("myDropdown_search").classList.toggle("show_search_studentgroup");
+}
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown_search");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
